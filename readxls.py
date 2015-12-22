c587d893ef9095adbbb31ca2acf5e894546e1914
wiltomap <wiltoma@gmail.com>
2015-12-22 21:35:51 +0100

    Finalisation de la lecture de la date

--- readxls.py
+++ readxls.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import xlrd
+import datetime
 
 
 xls_file1 = '/home/thomas/Documents/xls2pgsql/fepstest.xls'
 xls_file2 = 'D:\\python\\xls2pgsql\\fepstest.xls'
 
-wb = xlrd.open_workbook(xls_file2)
+wb = xlrd.open_workbook(xls_file1)
 
 ws = wb.sheet_by_name('Feuil1')
 
@@ -18,17 +19,29 @@ l = 1
 c = 0
 
 while l < nb_rows:
+
 	while c < nb_cols:
+
+		# Si on se trouve dans la colonne date
 		if c == 0:
-			date = xlrd.xldate_as_tuple(int(ws.row(l)[c]), 1)
-			print date
+
+			# datcell : instance de classe <class 'xlrd.sheet.Cell'>
+			datcell = ws.cell(l, c)
+
+			# dateps : stocke valeur de la cellule
+			dateps = ws.cell(l, c).value
+
+			# Stockage des éléments de la date
+			year, month, day, hour, minute, second = (xlrd.xldate_as_tuple(dateps, wb.datemode))
+
+			# Formatage de la date en yyy-mm-dd hh:mm:ss
+			dateps = datetime.datetime(year, month, day, hour, minute, second)
+
+			print dateps
+
 		else:
 			print ws.row(l)[c]
 		c += 1
 	c = 0
 	l += 1
-
-
-
-
-# print (wb.sheet_names())
+	
